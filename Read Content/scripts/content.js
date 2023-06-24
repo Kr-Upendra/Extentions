e5187a817b97e084229f3598dd9de7923d3c3438
Upendra Kumar <toxic74412@gmail.com>
2023-06-24 15:25:29 +0530

    Completed the read content extention

--- Read Content/scripts/content.js
+++ Read Content/scripts/content.js
@@ -1,51 +1,58 @@
-// const paragraph = document.querySelector(".about__abouttext p");
+const body = document.querySelector("body");
+const paragraphs = body.getElementsByTagName("p");
 
-// const speak = (text, voice, volume, pitch, rate) => {
-//   let speakData = new SpeechSynthesisUtterance();
-//   speakData.volume = volume;
-//   speakData.rate = rate;
-//   speakData.pitch = pitch;
-//   speakData.voice = voice;
-//   speakData.lang = "en";
-//   speakData.text = text;
-//   return speakData;
-// };
+const createAudioSymbol = () => {
+  const span = document.createElement("span");
+  span.textContent = "🔉";
+  span.style.cssText = `
+        color: inherit; font-size: inherit; font-weight: 500; cursor: pointer; margin-left: .85rem;
+    `;
+  return span;
+};
 
-// const getAllVoices = () => {
-//   let voices = speechSynthesis.getVoices();
-//   if (!voices.length) {
-//     let utterance = new SpeechSynthesisUtterance("");
-//     speechSynthesis.speak(utterance);
-//     voices = speechSynthesis.getVoices();
-//   }
-//   return voices;
-// };
+const speakWay = (text, volume, rate, pitch, voice) => {
+  let speakData = new SpeechSynthesisUtterance();
+  speakData.lang = "en";
+  speakData.rate = rate;
+  speakData.pitch = pitch;
+  speakData.voice = voice;
+  speakData.volume = volume;
+  speakData.text = text;
+  return speakData;
+};
 
-// const activateSpeak = (text) => {
-//   if ("speechSynthesis" in window) {
-//     let voices = getAllVoices();
-//     let rate = 1,
-//       pitch = 2;
-//     volume = 1;
-//     const doSpeak = speak(text, voices[5], volume, pitch, rate);
-//     return doSpeak;
-//   } else {
-//     alert("Your browser doesn't support speech synthesis 😞.");
-//   }
-// };
+const getAllVoices = () => {
+  let voices = speechSynthesis.getVoices();
+  if (!voices.length) {
+    let utterance = new SpeechSynthesisUtterance("");
+    speechSynthesis.speak(utterance);
+    voices = speechSynthesis.getVoices();
+  }
+  return voices;
+};
 
-// if (paragraph) {
-//   const audioBox = document.createElement("div");
-//   audioBox.textContent = "🔊";
-//   audioBox.style.cssText =
-//     "margin: 1rem auto; width: 30px; padding: 1rem; cursor: default; background-color: rgba(0, 0, 0, .45); backdrop-filter: blur(3px); border: 2px solid #5a5959; border-radius: .45rem;";
-//   paragraph.appendChild(audioBox);
-//   const speechableText = paragraph.textContent;
-//   const result = activateSpeak(speechableText);
+const activateSpeak = (text) => {
+  if ("speechSynthesis" in window) {
+    let voices = getAllVoices();
+    let rate = 1,
+      pitch = 2;
+    volume = 1;
+    const doSpeak = speakWay(text, volume, rate, pitch, voices[5]);
+    return doSpeak;
+  } else {
+    return alert("Your browser doesn't support speech synthesis 😞.");
+  }
+};
 
-//   audioBox.addEventListener("click", () => {
-//     speechSynthesis.speak(result);
-//   });
-// }
-
-console.log("Hello world!");
+if (body) {
+  for (const paragraph of paragraphs) {
+    if (paragraph.textContent.length >= 100) {
+      const span = createAudioSymbol();
+      paragraph.appendChild(span);
+      const result = activateSpeak(paragraph.textContent);
+      span.addEventListener("click", () => {
+        speechSynthesis.speak(result);
+      });
+    }
+  }
+}
